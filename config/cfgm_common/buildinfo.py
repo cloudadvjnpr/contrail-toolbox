97ba3d840002e23bab6fef49da6bc7eb020e85b6
root <root@5b4s37.contrail.juniper.net>
2020-04-07 21:18:23 -0700

    Revert "Revert "Add.""
    This reverts commit 26f5df4d82c5fc73f916ea192065a5f757dc97e3.

---
+++ config/cfgm_common/buildinfo.py
@@ -0,0 +1,1 @@
+build_info = "{\"build-info\" : [{\"build-version\" : \"1910\", \"build-time\" : \"2019-10-15 07:23:28.823462\", \"build-user\" : \"contrail-builder\", \"build-hostname\" : \"contrail-build-r1910-centos-23-generic-20191014233834\", ";
